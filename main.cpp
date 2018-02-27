a18b71896f9e0bda41b97689f99157dbd0459dd5
brianh618 <bherrera10@mail.csuchico.edu>
2018-02-27 11:43:28 -0800

    Grocery list manager that tells you your list after.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,53 @@
+//Brian Herrera
 #include<iostream>
 #include<string>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
-  int numItems = 0;
-  char input;
+        string list[5]; //array of 5 strings
+        int numItems = 0;
+        char input;
+        string item;
+        int ItemList;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+        do
+        {
 
-  return 0;
+                cout<<"\n==GROCERY LIST MANAGER==";
+                cout<<"\nEnter your choice: ";
+                cout<<"\n (A)dd an item";
+                cout<<"\n (Q)uit";
+                cout<<"\nYour choice (A/Q): ";
+                cin>>input;
+                if ((input == 'a' || input == 'A') )
+                {
+                        cout<<"What is the item?\n";
+                        cin >> item;
+
+                        if(numItems <5)
+                        {
+                                list[numItems]=item;
+                                numItems ++;
+                        }  
+
+
+                        if(numItems >=5)
+                        {
+                                cout<<"You'll need a bigger list!\n";
+                        }
+                }
+
+        }
+
+        while(input != 'q' && input != 'Q');
+        cout<<"==GROCERY LIST MANAGER==\n";
+        for(ItemList = 0; ItemList< 5; ItemList++)
+        {
+
+                cout<<ItemList+1<<" "<<list[ItemList]<<endl;
+
+        }
+
+        return 0;
 }
